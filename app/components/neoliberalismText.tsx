fd2b60a392d59aeb28b1f996e891fafb50ad7333
LinuxMistery00 <eduardosbarros.ed@gmail.com>
2024-09-21 19:35:39 -0300

    neoliberalism texto

--- app/components/neoliberalismText.tsx
+++ app/components/neoliberalismText.tsx
@@ -1,3 +1,12 @@
 export default function NeoLiberalismText() {
-    return <h1 className="m-4 font-mono text-center text-lg text-slate-950 ">Neoliberalism is Wonderful!</h1>
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="relative w-[max-content] font-mono flex items-center justify-center font-bold text-center text-4xl text-slate-950
+          before:absolute before:inset-0 before:animate-typewriter before:bg-white
+          after:absolute after:inset-0 after:w-[0.04em] after:h-[1.5em] after:top-1/2 after:translate-y-[-50%] after:animate-caret after:bg-black"
+        >
+          Neoliberalism is Wonderful!
+        </h1>
+      </div>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,27 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        typewriter: "typewriter 2s steps(100) forwards",
+        caret: "typewriter 2s steps(100) forwards, blink 1s steps(11) infinite 2s",
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%",
+          },
+        },
+        blink: {
+          "0%": { opacity: "0" },
+          "0.1%": { opacity: "1" },
+          "50%": { opacity: "1" },
+          "50.1%": { opacity: "0" },
+          "100%": { opacity: "0" },
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
